27f9ac4757539e9317e15e5ddaf98e6704094af7
dappernerddesigns <verity.gregory@gmail.com>
2021-12-30 23:18:49 +0000

    Trainer class created with a method to catch up to six pokemon

--- __tests__/trainers.test.js
+++ __tests__/trainers.test.js
@@ -6,7 +6,7 @@ describe('Trainers', () => {
     const player = new Trainer('Red')
 
     let actual = player
-    let expected = { name: 'Red', pokeBelt: {} }
+    let expected = { name: 'Red', pokeBelt: {}, slots: 6, storage: 0 }
 
     expect(actual).toEqual(expected)
   })
@@ -25,7 +25,7 @@ describe('Trainers', () => {
 
     let actual = player.pokeBelt
     let expected = {
-      Slot_one: {
+      '1': {
         name: 'Bulbasaur',
         hitPoints: 45,
         attackDmg: 16,
@@ -38,5 +38,57 @@ describe('Trainers', () => {
     }
 
     expect(actual).toEqual(expected)
+  })
+
+  test('catch method allows several pokemon to be caught', () => {
+    const player = new Trainer('Red')
+    const bulbasaur = new GrassType(
+      'Bulbasaur',
+      45,
+      16,
+      'Bul... Bulbasaur!',
+      'Razor Leaf',
+    )
+
+    const squirtle = new WaterType(
+      'Squirtle',
+      44,
+      16,
+      'Squ...Squirtle!',
+      'Surf',
+    )
+    const charmander = new FireType(
+      'Charmander',
+      44,
+      17,
+      'Cha...Charmander!',
+      'Flamethrower',
+    )
+
+    player.catch(bulbasaur)
+    player.catch(squirtle)
+    player.catch(charmander)
+
+    let actual = player.storage
+    let expected = 3
+
+    expect(actual).toBe(expected)
+  })
+
+  test('Pokebelt has 6 slots, players cannot add more pokemon', () => {
+    const player = new Trainer('Red')
+    const charmander = new FireType(
+      'Charmander',
+      44,
+      17,
+      'Cha...Charmander!',
+      'Flamethrower',
+    )
+    player.storage = 6
+
+    let actual = player.catch(charmander)
+    let expected = 'All slots on the PokeBelt are full!'
+
+    expect(actual).toBe(expected)
   })
 })
