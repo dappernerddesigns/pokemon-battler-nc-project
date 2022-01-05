ad6a52015416f5cab84ac2a74899c1debf1d01a6
dappernerddesigns <verity.gregory@gmail.com>
2022-01-05 19:35:17 +0000

    battle function almost complete

--- __tests__/battle.test.js
+++ __tests__/battle.test.js
@@ -28,12 +28,52 @@ describe('Battle', () => {
     const playerOne = new Trainer('Red')
     const playerTwo = new Trainer('Blue')
 
-    let testBattle = new Battle(playerOne, playerTwo)
     playerOne.catch(charmander)
-    playerTwo.catch(charmander)
+    playerTwo.catch(flareon)
+    let testBattle = new Battle(playerOne, playerTwo)
+    testBattle.fight()
+
     charmander.useYourMoves()
+    flareon.useYourMoves()
+    let actual = flareon.hitPoints
+    let expected = 48
+
+    expect(actual).toBe(expected)
+  })
+
+  test('A fainted pokemon will log hitpoints at zero', () => {
+    const playerOne = new Trainer('Red')
+    const playerTwo = new Trainer('Blue')
+
+    playerOne.catch(charmander)
+    playerTwo.catch(flareon)
+
+    let testBattle = new Battle(playerOne, playerTwo)
+
+    testBattle.fight()
+    testBattle.fight()
+    testBattle.fight()
+    testBattle.fight()
+
     let actual = charmander.hitPoints
-    let expected = 27
+    let expected = 0
+
+    expect(actual).toBe(expected)
+  })
+
+  test.only('Strengths and weaknesses are calculated correctly', () => {
+    const playerOne = new Trainer('Red')
+    const playerTwo = new Trainer('Blue')
+
+    playerOne.catch(charmander)
+    playerTwo.catch(bulbasaur)
+
+    let testBattle = new Battle(playerOne, playerTwo)
+
+    testBattle.fight()
+
+    let actual = bulbasaur.hitPoints
+    let expected = 24
 
     expect(actual).toBe(expected)
   })
